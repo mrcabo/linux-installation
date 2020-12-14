974aa0faa2241912e65bb6d1cc891111f723937c
mrcabo <dcgdiego@gmail.com>
2020-12-14 01:54:42 +0100

    gnome G Drive integration works fine

--- ubuntu-apps.sh
+++ ubuntu-apps.sh
@@ -10,6 +10,7 @@ sudo apt --yes install tilix
 sudo apt --yes install vlc
 sudo apt --yes install krita
 sudo apt --yes install nano
+sudo apt --yes install vim
 sudo apt --yes install tree
 sudo apt --yes install htop
 
@@ -21,9 +22,8 @@ echo 'alias python=python3' >> $HOME/.bashrc
 # sudo apt update
 # sudo apt install grub-customizer
 
-# Installing gdrive
-curl https://dl.google.com/go/go1.13.5.linux-amd64.tar.gz --output ~/Downloads/go1.13.5.linux-amd64.tar.gz
-sudo tar -C /usr/local -xzf ~/Downloads/go1.13.5.linux-amd64.tar.gz
-echo 'export PATH=$PATH:/usr/local/go/bin' >> $HOME/.profile
-go get -u github.com/odeke-em/drive/cmd/drive
-echo 'export PATH=$PATH:/home/diego/go/bin' >> $HOME/.bashrc
+# Installing gdrive - Looks like gnome integrations works fine now..?
+# sudo apt --yes install golang-go
+# go get -u github.com/odeke-em/drive/cmd/drive
+# echo 'export GOPATH=$HOME/go' >> $HOME/.bashrc
+# echo 'export PATH=$PATH:$GOPATH:$GOPATH/bin' >> $HOME/.bashrc
